92e8a78abf8862f0f0d2692c7675113e5c89a108
Saidshahahmadi25 <saidshahahmadi2016@gmail.com>
2026-03-24 22:12:24 +0430

    Refactor component imports in App to use centralized index file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import HeroSection from "./components/HeroSection/HeroSection";
-import ConsultationSection from "./components/ConsultationSection/ConsultationSection";
-import WhyChooseSection from "./components/WhyChooseSection/WhyChooseSection";
-import ServicesSection from "./components/ServicesSection/ServicesSection";
-import AboutSection from "./components/AboutSection/AboutSection";
-import FeaturesSection from "./components/FeaturesSection/FeaturesSection";
-import HowItWorksSection from "./components/HowItWorksSection/HowItWorksSection";
-import TestimonialsSection from "./components/TestimonialsSection/TestimonialsSection";
-import BlogSection from "./components/BlogSection/BlogSection";
-import FAQSection from "./components/FAQSection/FAQSection";
-import ContactFooterSection from "./components/ContactFooterSection/ContactFooterSection";
+import {
+  HeroSection,
+  ConsultationSection,
+  WhyChooseSection,
+  ServicesSection,
+  AboutSection,
+  FeaturesSection,
+  HowItWorksSection,
+  TestimonialsSection,
+  BlogSection,
+  FAQSection,
+  ContactFooterSection
+} from './components';
+
 
 export default function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+// components/index.js
+import HeroSection from "./HeroSection/HeroSection";
+import ConsultationSection from "./ConsultationSection/ConsultationSection";
+import WhyChooseSection from "./WhyChooseSection/WhyChooseSection";
+import ServicesSection from "./ServicesSection/ServicesSection";
+import AboutSection from "./AboutSection/AboutSection";
+import FeaturesSection from "./FeaturesSection/FeaturesSection";
+import HowItWorksSection from "./HowItWorksSection/HowItWorksSection";
+import TestimonialsSection from "./TestimonialsSection/TestimonialsSection";
+import BlogSection from "./BlogSection/BlogSection";
+import FAQSection from "./FAQSection/FAQSection";
+import ContactFooterSection from "./ContactFooterSection/ContactFooterSection";
+
+export {
+  HeroSection,
+  ConsultationSection,
+  WhyChooseSection,
+  ServicesSection,
+  AboutSection,
+  FeaturesSection,
+  HowItWorksSection,
+  TestimonialsSection,
+  BlogSection,
+  FAQSection,
+  ContactFooterSection,
+};
